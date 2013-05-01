d628af16cef037d2da79fdbbe7e4bc26c052f4fd
arrblair <ryan@livefyre.com>
2013-04-30 18:03:52 -0700

    instantiate list for hist_text

--- context.py
+++ context.py
@@ -3,6 +3,7 @@ import os
 hist_file_line_count = os.system('wc -l ~/.bash_history')
 print hist_file_line_count 
 
+hist_text = []
 hist_text = os.system('cat ~/.bash_history')
 print hist_text
 print len(hist_text)
